65886f9afe715ffc592a0526db24d8404730aafe
marcusp619 <marcusp619@gmail.com>
2018-08-27 15:28:57 -0600

    Initial setup for Trie-test and pseduocode

---
+++ test/trie-test.js
@@ -0,0 +1,17 @@
+import { assert } from 'chai';
+    
+describe('Trie', function() {
+  it('should be a function', function() {
+    assert.equal(true, true);
+  });
+
+
+  // insert method
+    //take a word
+    //lowercase it
+    //split the word into letters
+    //if letter is not node create a node and then shift off first letter
+    //the letter is a node skip and check next one
+    
+
+});
