1e04d47b98821fa8e1aada3f40f144ebd66fda2d
marcusp619 <marcusp619@gmail.com>
2018-08-27 21:03:55 -0600

    Insert method with comments

--- test/trie-test.js
+++ test/trie-test.js
@@ -1,10 +1,36 @@
-import { assert } from 'chai';
-    
-describe('Trie', function() {
-  it('should be a function', function() {
-    assert.equal(true, true);
+import { assert, expect } from 'chai';
+import Trie from '../lib/Trie';
+import Node from '../lib/Node';
+
+
+describe('Trie', () => {
+  let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
   });
 
+  it('should be a function', () => {
+    assert.isFunction(Trie);
+  });
+
+  it('should set its default head to null', () => {
+    expect(trie.head).to.eq(null);
+  });
+
+  // describe('insert', () => {
+  //   it.skip('should take in a word as an argument', () => {
+  //     // assert.equal(new Trie(data), )
+  //   });
+  //   it.skip('should change all words into lowercase', () => {
+
+  //   });
+  //   it.skip('should take the word and make an array', () => {
+
+  //   });
+
+  // });
+});
 
   // insert method
     //take a word
@@ -12,6 +38,6 @@ describe('Trie', function() {
     //split the word into letters
     //if letter is not node create a node and then shift off first letter
     //the letter is a node skip and check next one
-    
+
 
 });
