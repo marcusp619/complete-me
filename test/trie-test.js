4f856a959e1497013a039401ba8c2624bb917664
marcusp619 <marcusp619@gmail.com>
2018-08-27 21:25:44 -0600

    passed function and parameter test

--- test/trie-test.js
+++ test/trie-test.js
@@ -18,18 +18,18 @@ describe('Trie', () => {
     expect(trie.head).to.eq(null);
   });
 
-  // describe('insert', () => {
-  //   it.skip('should take in a word as an argument', () => {
-  //     // assert.equal(new Trie(data), )
-  //   });
-  //   it.skip('should change all words into lowercase', () => {
+  describe('insert', () => {
+    it('should take in a word as an argument', () => {
+      assert.equal(trie.insert('hey'));
+    });
+    it('should change all words into lowercase', () => {
+      assert.equal(trie.insert('hey'), ['h', 'e', 'y']);
+    });
+    it.skip('should take the word and make an array', () => {
 
-  //   });
-  //   it.skip('should take the word and make an array', () => {
+    });
 
-  //   });
-
-  // });
+  });
 });
 
   // insert method
@@ -38,6 +38,3 @@ describe('Trie', () => {
     //split the word into letters
     //if letter is not node create a node and then shift off first letter
     //the letter is a node skip and check next one
-
-
-});
